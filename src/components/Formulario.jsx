c8d4124d2b1bd4fc26823717fcee2328c1233489
fister <fistergutierrez@gmail.com>
2023-12-27 14:53:16 -0600

    componente formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>
-      <h2>formulario</h2>
+    <div className='md:w-1/2 lg:w-2/5'>
+      <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
+
+      <p className='text-lg mt-5 text-center'>
+        Anade Pacientes y {''}
+        <span className='text-indigo-600 font-bold text-lg'>Administralos</span>
+      </p>
     </div>
   )
 }
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ListadoPacientes() {
   return (
-    <div>
+    <div className='w-1/2 lg:w-3/5'>
       <h2>Listado Pacientes</h2>
     </div>
   )
