ccba9b2c678a69dd4d00aa1d2fc78b31eb0a4f0e
fister <fistergutierrez@gmail.com>
2023-12-27 20:19:14 -0600

    desing listado de pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,36 @@
+export default function Paciente() {
+    return (
+        <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Nombre: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Propietario: {''}
+                <span className='font-normal normal-case'>Cristian Yair</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Email: {''}
+                <span className='font-normal normal-case'>fister@gmail.com</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Fecha Alta: {''}
+                <span className='font-normal normal-case'>27 de diciembre del 2023</span>
+            </p>
+
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Sintomas: {''}
+                <span className='font-normal normal-case'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum odio error inventore aut doloribus facilis doloremque recusandae unde est? Ratione magni sequi facere, ipsam inventore in sapiente excepturi nam quis?</span>
+            </p>
+        </div>
+    )
+}
+
+
+
+
+
+
